09c8a9ea8201b1cfae851125571069bb450e8bb5
root <root@ip-172-31-30-221.ap-south-1.compute.internal>
2016-08-06 09:06:45 +0000

    modification in files

--- wp-config.php
+++ wp-config.php
@@ -15,8 +15,10 @@
  * @package WordPress
  */
 
-     define('WP_HOME', 'http://wordpress-1634645544.ap-south-1.elb.amazonaws.com');
+ /*    define('WP_HOME', 'http://wordpress-1634645544.ap-south-1.elb.amazonaws.com');
      define('WP_SITEURL', 'http://wordpress-1634645544.ap-south-1.elb.amazonaws.com');
+*/
+
 
       define('WP_REDIS_HOST', 'wordpress.dbuclp.0001.aps1.cache.amazonaws.com');
      define('FS_METHOD', 'direct');
